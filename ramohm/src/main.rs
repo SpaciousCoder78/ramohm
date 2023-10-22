5a67b72dab044d5415d76ddfe7b3f323639651c4
SpaciousCoder78 <88923986+SpaciousCoder78@users.noreply.github.com>
2023-10-22 08:28:53 +0530

    added some more code

--- ramohm/src/main.rs
+++ ramohm/src/main.rs
@@ -1,3 +1,22 @@
+//importing libraries
+use std::io;
+
+//main function
 fn main() {
-    println!("Hello, world!");
+    //calling menu function
+    menu();
+}
+//menu function
+fn menu(){
+    //menu function menu
+    println!("------------------------RamOhm--------------------------");
+    println!("------------------------v1.0-----------------------------");
+    println!("------------------------Menu-----------------------------");
+    println!("1. Calculate Resistance");
+    println!("2. Calculate Current");
+    println!("3. Calculate Voltage");
+    println!("4. Calculate Power");
+    println!("5. Calculate Energy");
+    println!("6. Exit");
+
 }
