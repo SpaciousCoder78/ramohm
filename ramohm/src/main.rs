b5e4d289ef021aab2317226c2d5fb4623fab1f83
SpaciousCoder78 <88923986+SpaciousCoder78@users.noreply.github.com>
2023-10-23 10:13:27 +0530

    fix i32 type issue

--- ramohm/src/main.rs
+++ ramohm/src/main.rs
@@ -18,5 +18,36 @@ fn menu(){
     println!("4. Calculate Power");
     println!("5. Calculate Energy");
     println!("6. Exit");
+    println!("Enter your choice: ");
+    //declaring choice variable 
+    let mut choice = String::new();
+    //reading input from user
+    io::stdin().read_line(&mut choice).expect("Failed to read input");
+    let numchoice:i32 = choice.trim().parse();
+
+    match numchoice{
+        1 => calcresistance(),
+        2 => calccurrent(),
+        3 => calcvoltage(),
+        4 => calcpower(),
+        5 => calcenergy(),
+        6 => println!("Exiting..."),
+        _ => println!("Invalid choice"),
+    }
+
+}
+fn calcresistance(){
+    println!("test");
+}
+fn calccurrent(){
+
+}
+fn calcvoltage(){
+
+}
+fn calcpower(){
+
+}
+fn calcenergy(){
 
 }
